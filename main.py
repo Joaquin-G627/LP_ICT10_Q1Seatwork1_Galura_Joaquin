eee44048f424a7caba3fe0cdf4f9c9a2a6e502a1
Joaquin-G627 <galura.rs@obmontessori.edu.ph>
2026-09-04 07:52:45 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+studname = "Ciana Dicka" #String
+studeage = 15 #Integer
+stud_height = 150 #Integer
+stud_country_to_visit = ["Australia", "South Korea", "Japan"] #List
+stud_new = False #Boolean
+stud_info = {
+    "color": "red", 
+    "car_brand": "Tesla", 
+    "shoe_size": 7, 
+    "bff": "Jade"
+    } #Dictionary
+stud_favefruits = {
+    "Strawberry",
+    "Grapes",
+    "Bananas",
+    "Mangoes",
+    "Oranges"
+} #Set
+days_of_week = (
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday", 
+    "Sunday"
+    ) #Tuple
+
+print("Name:", studname)
+print("Age:", studeage)
+print("Height:", stud_height)
